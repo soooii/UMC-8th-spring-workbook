97b6b959fe756695352bdcfc9b9f9b4de9f5167d
songhee <songhee9327@gmail.com>
2025-05-01 11:15:55 +0900

    settings : inital commit

---
+++ src/main/java/com/hufs/umc5/Umc5Application.java
@@ -0,0 +1,13 @@
+package com.hufs.umc5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Umc5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hufs/umc5/Umc5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hufs.umc5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
